1704002ddf5b724252ba79e03506eb3420d327fa
Edwards Moses <edwardsmoses3@gmail.com>
2020-12-14 16:29:48 +0100

    numpy array

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ import numpy as np
 
 msg = "hello world";
 print(msg);
+
+#init the numpy array
+array = np.array([-1,2,5], dtype=np.float);
+
+#print the array representation
+print(repr(array));
+
+print(array);
