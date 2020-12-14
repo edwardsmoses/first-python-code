7cc173b8a0b024ad7c68268b503f88f9159a7bc9
Edwards Moses <edwardsmoses3@gmail.com>
2020-12-14 16:44:50 +0100

    learnt about arrays..
    it uses the same concept as javascript at-least on the subject of arrays

--- hello.py
+++ hello.py
@@ -10,3 +10,29 @@ array = np.array([-1,2,5], dtype=np.float);
 print(repr(array));
 
 print(array);
+
+newarray = np.array([[0,1,2], [3,4,5]], dtype=np.int);
+print(repr(newarray));
+
+print(newarray);
+
+floatarray = np.array([1,2.34,3,5]);
+print(floatarray);
+
+stringarray = np.array([1,34,42, "eifoj", "ojeiof"]);
+print(stringarray);
+
+
+a = np.array([0,1]);
+b = np.array([9,8]);
+
+c = a;
+print('Array a: {}', format(repr(a)));
+
+c[0] = 5;
+print('Array a: {}', format(repr(a)));
+
+d = b.copy();
+d[0] = 6;
+print('Array b : {}', format(repr(b)));
+print('Array d : {}', format(repr(d)));
