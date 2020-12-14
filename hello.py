141f958005a302d094f99394385b8aa5ce36e2a8
Edwards Moses <edwardsmoses3@gmail.com>
2020-12-14 16:45:54 +0100

    added comments

--- hello.py
+++ hello.py
@@ -16,12 +16,16 @@ print(repr(newarray));
 
 print(newarray);
 
+
+##upcasting arrays
 floatarray = np.array([1,2.34,3,5]);
 print(floatarray);
 
 stringarray = np.array([1,34,42, "eifoj", "ojeiof"]);
 print(stringarray);
 
+
+##copying arrays
 
 a = np.array([0,1]);
 b = np.array([9,8]);
@@ -36,3 +40,5 @@ d = b.copy();
 d[0] = 6;
 print('Array b : {}', format(repr(b)));
 print('Array d : {}', format(repr(d)));
+
+
